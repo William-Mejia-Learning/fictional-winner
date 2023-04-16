b67531d4bcb1005d189365e86d36a74bc9067355
williamxmejia <willx1995@gmail.com>
2023-04-16 15:54:03 -0500

    added a bush image

--- main_game.py
+++ main_game.py
@@ -19,7 +19,15 @@ while running:
     sand_turf = pygame.draw.rect(screen, (194, 178, 128), pygame.Rect(500,0,500,500))
     water_surf = pygame.draw.rect(screen, '#00FFFF', pygame.Rect(850,0, 150,500))
     player = pygame.draw.circle(screen, 'White', player_pos, 10)
+    
+    bush_list = []
+    bush_surf = pygame.image.load('assets/PNG/Assets/Bush_simple1_1.png').convert_alpha()
+    bush_rect = bush_surf.get_rect(midbottom=(80,300))
 
+    screen.blit(bush_surf, bush_rect)
+    
+    
+    screen.blit(bush_surf, bush_rect)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
