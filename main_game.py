564901852701f44bb2d4f9c08aa8123fab62dc39
williamxmejia <willx1995@gmail.com>
2023-04-25 23:29:38 -0500

    feat:added files

--- main_game.py
+++ main_game.py
@@ -18,8 +18,21 @@ while running:
     grass_surf = pygame.draw.rect(screen, 'Green', pygame.Rect(0,0,500,500))
     sand_turf = pygame.draw.rect(screen, (194, 178, 128), pygame.Rect(500,0,500,500))
     water_surf = pygame.draw.rect(screen, '#00FFFF', pygame.Rect(850,0, 150,500))
-    player = pygame.draw.circle(screen, 'White', player_pos, 10)
-    
+    #player = pygame.draw.circle(screen, 'White', player_pos, 10)
+
+    player_surf_dict = {
+            'player_surf_down': pygame.image.load('assets/down.png').convert_alpha(),
+            'player_surf_up': pygame.image.load('assets/up.png').convert_alpha(),
+            'player_surf_left': pygame.image.load('assets/left.png').convert_alpha(),
+            'player_surf_right': pygame.image.load('assets/right.png').convert_alpha(),
+            }
+    player_rect_dict = {
+            'player_rect_down': player_surf_dict['player_surf_down'].get_rect(midbottom=(20,20)),
+            'player_rect_up': player_surf_dict['player_surf_up'].get_rect(midbottom=(0,0)),
+            'player_rect_left': player_surf_dict['player_surf_left'].get_rect(midbottom=(0,0)),
+            'player_rect_right': player_surf_dict['player_surf_right'].get_rect(midbottom=(0,0)),
+            }
+
     bush_surf = pygame.image.load('assets/PNG/Assets/Bush_simple1_1.png').convert_alpha()
     bush_2_surf = pygame.image.load('assets/PNG/Assets/Burned_tree1.png').convert_alpha()
     
@@ -42,6 +55,8 @@ while running:
     screen.blit(bush_2_surf, bush2_rect)
     
     
+    pygame.draw.rect(screen,
+    print(player_surf_dict)
     
 
     keys = pygame.key.get_pressed()
